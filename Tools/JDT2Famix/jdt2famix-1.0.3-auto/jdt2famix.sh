c0d9f2dce55613add2b36b6a95fb2f421a7291ce
Pedro Moraes <pedro_hmoraes@hotmail.com>
2018-10-03 11:26:10 -0400

    Added missing JDT2FAMIX atuomatized for various MSEs

---
+++ Tools/JDT2Famix/jdt2famix-1.0.3-auto/jdt2famix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#This is the main script with which to trigger jdt2famix from the command line.
+#How to trigger
+#- go to the path where the system is (the path should include both the sources and the jars)
+#- execute /path/to/jdt2famix.sh
+#- the result is an MSE file with the name of the folder from which the script was executed
+
+BASE_PATH="$(cd "$(dirname "$0")" && pwd)"
+echo $BASE_PATH/*:$BASE_PATH
+java -cp "C:/Users/Pedro Henrique/Documents/IC/JDT2Famix/jdt2famix-1.0.3/*" com.feenk.jdt2famix.injava.Main
